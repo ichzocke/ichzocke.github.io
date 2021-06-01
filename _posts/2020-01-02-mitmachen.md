9b54d1bfcbb77feccaf1f16d009778bcab503328
Dominik Moser <severus.drakone@gmail.com>
2021-06-01 07:09:21 +0200

    non forward form

--- _posts/2020-01-02-mitmachen.md
+++ _posts/2020-01-02-mitmachen.md
@@ -15,6 +15,7 @@ fa-icon: user
 </div>
 
 <div class="container">
+    <h1 id="signupsuccess">Vielen Dank für deine Teilnahme!</h1>
     <form id="signup" target="_blank" action="https://formsubmit.co/3c6ea96846d936d717eeca43bb0bfe0e" method="POST">
         <div class="six columns">
             <label for="name">Name:</label>
@@ -26,11 +27,13 @@ fa-icon: user
         </div>
         <label for="message">Willst du uns noch etwas sagen?</label>
         <textarea id="message" class="u-full-width" name="message" placeholder="Hi, bitte kontaktiert mich lieber unter folgender Telefonnummer..." ></textarea>
-        <input class="button-primary u-full-width" type="submit" value="Anmelden">
+        <input id="signupbutton" class="button-primary u-full-width" type="submit" value="Anmelden">
         <input type="text" name="_honey" style="display:none">
+        <input type="hidden" name="_subject" value="[Mitmachen]">
         <input type="hidden" name="_template" value="table">
         <input type="hidden" name="_captcha" value="false">
     </form>
+    <div id="signuperror" class="container center"></div>
 </div>
 
 #### Teilnahmebedingungen
--- site.js
+++ site.js
@@ -93,3 +93,45 @@ $(document).ready(function (){
 
 });
 
+$("#signup").submit(function( event ) {
+    event.preventDefault();
+    $("#signupbutton").prop("disabled",true);
+    var target = event.currentTarget.action
+    var identifier = target.substring(target.lastIndexOf('/') + 1);
+    $.ajax({
+        method: 'POST',
+        url: 'https://formsubmit.co/ajax/' + identifier,
+        dataType: 'json',
+        accepts: 'application/json',
+        data: {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val(),
+            _subject: "[Mitmachen]",
+            _template: "table"
+        },
+        success: (data) => {
+            console.log(data);
+            if (true == data.success) {
+                signupSuccess()
+            } else {
+                signupError()
+            }
+        },
+        error: (err) => errorMessage()
+    });
+});
+
+function signupSuccess() {
+    $("#signuperror").fadeOut("fast");
+    $("#signup").fadeTo(500, 0);
+    $("#signupsuccess").css('z-index', 999).fadeTo(500, 1);
+}
+
+function signupError() {
+    var error = $("#signuperror");
+    error.html(atob("RmVobGVyIGJlaW0gYW5tZWxkZW4sIGJpdHRlIHZlcnN1Y2hlIGVzIHNwJmF1bWw7dGVyIG5vY2htYWwgb2RlciBzZW5kZSBlaW5lIGVtYWlsIGFuIDxhIGhyZWY9Im1haWx0bzptaXRtYWNoZW5AaWNoem9ja2UuYXQ/c3ViamVjdD1bTWl0bWFjaGVuXSI+bWl0bWFjaGVuQGljaHpvY2tlLmF0PC9hPg=="));
+    error.fadeIn(500);
+    $('html,body').animate({scrollTop: error.offset().top - 170}, 500, "easeInOutExpo");
+    $("#signupbutton").prop("disabled",false);
+}
